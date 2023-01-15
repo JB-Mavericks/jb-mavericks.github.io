334e3f27b975dca29b9b7a10a0f5d4cdc5e0ff62
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-01-15 12:37:10 +0600

    tech section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Info from './components/Info';
+import Techs from './components/Techs';
 // import Erd from './components/Erd';
-// import Techs from './components/Techs';
 // import Ui from './components/Ui';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <div className='xl:mx-32 lg:mx-24 md:mx-16 sm:mx-11 mx-9'>
       <Hero />
       <Info />
-      {/* <Techs /> */}
+      <Techs />
       {/* <Ui /> */}
       {/* <Erd /> */}
       </div>
---
+++ src/components/Techs.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+//images
+import python from './../images/python.svg'
+import lucid from './../images/lucid.svg'
+import github from './../images/github.svg'
+import azure from './../images/azure.svg'
+import flutter from './../images/flutter.svg'
+import figma from './../images/figma.svg'
+import django from './../images/django.svg'
+import dart from './../images/dart.svg'
+
+const Techs = () => {
+     return (
+          <div className='pt-36' id='tech'>
+               <h1 className='font-roboto xl:font-black md:font-bold font-semibold text-white xl:text-6xl md:text-4xl text-3xl text-center mb-12'>Tech Stack</h1>
+               <div className='grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-y-12 content-center justify-items-center'>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={dart} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={flutter} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={python} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={azure} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={django} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={figma} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={github} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+                    <div className='w-28 sm:w-36 h-28 sm:h-36 bg-blackLight rounded-xl flex flex-col justify-center items-center'>
+                         <img src={lucid} alt='Dart' className='w-auto h-auto' />
+                         <p className='text-white font-montserrat font-semibold text-2xl'>Dart</p>
+                    </div>
+               </div>
+          </div>
+     )
+}
+
+export default Techs
