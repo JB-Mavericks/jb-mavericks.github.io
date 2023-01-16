c6c65ab516ccde73c9fe48944ea0822301d0f840
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-01-16 19:17:04 +0600

    img responsive done

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -9,28 +9,28 @@ const Blogs = () => {
      <h1 className='font-roboto xl:font-black md:font-bold font-semibold text-white xl:text-6xl md:text-4xl text-3xl text-center mb-12'>Blogs</h1>
      <div className='grid grid-cols-2 xl:gap-x-20 gap-x-14 gap-y-12'>
           <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
-               <img src={blog} alt="blog"/>
+               <img src={blog} alt="blog" className='h-full w-auto'/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
                     <p className='font-roboto xl:font-black md:font-bold font-semibold text-sm text-opacityblack'>10th January, 2023</p>
                </div>
           </div>
           <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
-               <img src={blog} alt="blog"/>
+               <img src={blog} alt="blog" className='h-full w-auto'/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
                     <p className='font-roboto xl:font-black md:font-bold font-semibold text-sm text-opacityblack'>10th January, 2023</p>
                </div>
           </div>
           <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
-               <img src={blog} alt="blog"/>
+               <img src={blog} alt="blog" className='h-full w-auto'/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
                     <p className='font-roboto xl:font-black md:font-bold font-semibold text-sm text-opacityblack'>10th January, 2023</p>
                </div>
           </div>
           <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
-               <img src={blog} alt="blog"/>
+               <img src={blog} alt="blog" className='h-full w-auto'/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
                     <p className='font-roboto xl:font-black md:font-bold font-semibold text-sm text-opacityblack'>10th January, 2023</p>
