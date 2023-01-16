b3547b266b0e791e2d0d758390c3bdf2deded3e6
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-01-16 19:14:18 +0600

    blog responsive done

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -7,29 +7,29 @@ const Blogs = () => {
   return (
     <div className='pt-36'>
      <h1 className='font-roboto xl:font-black md:font-bold font-semibold text-white xl:text-6xl md:text-4xl text-3xl text-center mb-12'>Blogs</h1>
-     <div className='grid grid-cols-2 gap-x-20 gap-y-12'>
-          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center p-10 gap-10'>
+     <div className='grid grid-cols-2 xl:gap-x-20 gap-x-14 gap-y-12'>
+          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
                <img src={blog} alt="blog"/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
                     <p className='font-roboto xl:font-black md:font-bold font-semibold text-sm text-opacityblack'>10th January, 2023</p>
                </div>
           </div>
-          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center p-10 gap-10'>
+          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
                <img src={blog} alt="blog"/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
                     <p className='font-roboto xl:font-black md:font-bold font-semibold text-sm text-opacityblack'>10th January, 2023</p>
                </div>
           </div>
-          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center p-10 gap-10'>
+          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
                <img src={blog} alt="blog"/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
                     <p className='font-roboto xl:font-black md:font-bold font-semibold text-sm text-opacityblack'>10th January, 2023</p>
                </div>
           </div>
-          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center p-10 gap-10'>
+          <div className='bg-blog rounded-2xl grid grid-cols-2 justify-center items-center xl:p-10 lg:p-8 md:p-6 p-4 xl:gap-10 lg:gap-8 md:gap-6 gap-4'>
                <img src={blog} alt="blog"/>
                <div>
                     <h2 className='font-roboto xl:font-black md:font-bold font-semibold text-white lg:text-2xl md:text-xl text-lg'>New version of Jeetubhaiya is coming soon</h2>
