8ad07a3aeb715b4232fc785231e97fe1a61c7b54
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-01-16 22:47:16 +0600

    maverick done

--- src/components/mavericks/Mavericks.js
+++ src/components/mavericks/Mavericks.js
@@ -6,7 +6,7 @@ import Marzan from './Marzan'
 const Mavericks = () => {
      return (
           <div>
-               <div className='grid grid-cols-3 justify-items-stretch'>
+               <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
                     <Alexpy />
                     <Adib />
                     <Marzan />
